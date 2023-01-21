fcf649c1d7ccb9d150a6b00d23f5a737ab055dfb
Aline Mizumukai <aline.mizumukai@mag-it.com.br>
2023-01-20 21:08:23 -0300

    Add Classe Aluguel e dados para teste na Aplicacao

---
+++ src/Aluguel.java
@@ -0,0 +1,34 @@
+import Interfaces.Cliente;
+
+public class Aluguel {
+
+    private Veiculo veiculo;
+    private Cliente cliente;
+    private int dias;
+
+    public Aluguel(Veiculo veiculo, Cliente cliente, int dias) {
+        this.veiculo = veiculo;
+        this.cliente = cliente;
+        this.dias = dias;
+    }
+
+    public void calcularValorFinal(){
+        double totalDiaria = calcularTotalBruto();
+        double desconto = calcularTotalDesconto(totalDiaria);
+        System.out.printf("\n.:: EXTRATO DA LOCAÇÃO ::." +
+                        "\nCLIENTE = %s" +
+                        "\nVEICULO = %s" +
+                        "\nQUANTIDADE DE DIÁRIAS = %d" +
+                        "\nTOTAL BRUTO = R$ %.2f" +
+                        "\nDESCONTO = R$ %.2f" +
+                        "\nVALOR TOTAL DO ALUGUEL = R$ %.2f\n", cliente, veiculo, dias, totalDiaria, desconto, (totalDiaria - desconto));
+    }
+
+    private double calcularTotalBruto(){
+        return veiculo.getTipoVeiculo().getPrecoBase() * dias;
+    }
+
+    private double calcularTotalDesconto(double valor){
+        return valor * cliente.retornarDesconto(dias);
+    }
+}
--- src/Aplicacao.java
+++ src/Aplicacao.java
@@ -1,5 +1,28 @@
+import Enums.TipoVeiculo;
+import Interfaces.Cliente;
+
 public class Aplicacao {
     public static void main(String[] args) {
+
+        // Veículos para teste
+        Veiculo veiculo1 = new Veiculo("NAX7600", TipoVeiculo.PEQUENO);
+        Veiculo veiculo2 = new Veiculo("JVQ2356", TipoVeiculo.MEDIO);
+        Veiculo veiculo3 = new Veiculo("HTH2387", TipoVeiculo.SUV);
+
+        // Clientes para teste
+        Cliente cliente1 = new PessoaFisica("317.603.143-23", "Emily Isabella Ramos");
+        Cliente cliente2 = new PessoaJuridica("37.755.541/0001-30", "Tânia e Bruno Assessoria Jurídica ME");
+
+        // TESTE
+
+        // Dados entrada
+        Cliente cliente = cliente1;
+        Veiculo veiculo = veiculo1;
+        int numeroDiarias = 6;
+
+        // Execução
+        Aluguel locacao = new Aluguel(veiculo, cliente, numeroDiarias);
+        locacao.calcularValorFinal();
 
     }
 }
